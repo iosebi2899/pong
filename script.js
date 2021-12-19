51decac82744eca5735f45df1ffb7715efdf30e3
iosebi2899 <ioseb.geliashvili@geolab.edu.ge>
2021-12-19 16:46:34 +0400

    GG

---
+++ script.js
@@ -0,0 +1,44 @@
+import Ball from "./ball.js";
+import Paddle from "./paddle.js";
+
+const ball = new Ball(document.getElementById("ball"));
+const playerPaddle = new Paddle(document.getElementById("player-paddle"));
+const computerPaddle = new Paddle(document.getElementById("computer-paddle"));
+const playerScoreElem = document.getElementById("player-score");
+const computerScoreElem = document.getElementById("computer-score");
+
+let lastTime;
+const update = (time) => {
+  if (lastTime != null) {
+    const delta = time - lastTime;
+
+    ball.update(delta, [playerPaddle.rect(), computerPaddle.rect()]);
+    computerPaddle.update(delta, ball.y);
+
+    if (isLose()) handleLose();
+  }
+  lastTime = time;
+  window.requestAnimationFrame(update);
+};
+
+const handleLose = () => {
+  const rect = ball.rect();
+  if (rect.right >= window.innerWidth) {
+    playerScoreElem.textContent = parseInt(playerScoreElem.textContent) + 1;
+  } else {
+    computerScoreElem.textContent = parseInt(computerScoreElem.textContent) + 1;
+  }
+  ball.reset();
+  computerPaddle.reset();
+};
+
+const isLose = () => {
+  const rect = ball.rect();
+  return rect.right >= window.innerWidth || rect.left <= 0;
+};
+
+document.addEventListener("mousemove", (e) => {
+  playerPaddle.position = (e.y / window.innerHeight) * 100;
+});
+
+window.requestAnimationFrame(update);
